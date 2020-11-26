9eccc8657dce9f2d0bc9b0e55bf9f005b6feec37
aobatact <aobatact144@gmail.com>
2020-11-26 11:47:40 +0900

    change exp

--- src/float_ops.rs
+++ src/float_ops.rs
@@ -1,47 +1,6 @@
 use crate::ops::*;
 use crate::*;
-use core::ops::{Add, Div, Mul, Neg, Sub};
 use num_traits::float::Float;
-
-/*
-/// [`UnaryOp`](`crate::UnaryOp`) marker for [`exp`](`num_traits::float::Float::exp`) with [`ExpSym`](`crate::float_ops::ExpSym`) for float
-#[derive(Copy, Clone, PartialEq, Eq, Debug, Default)]
-pub struct ExpOp;
-impl UnaryOp for ExpOp {}
-/// Represent Unary[`exp`](`num_traits::float::Float::exp`) Symbol for float
-/// ```
-/// # use symbolic_diffs::*;
-/// # use symbolic_diffs::float_ops::*;
-/// # use typenum::*;
-/// # use generic_array::*;
-/// let x = DimMonomial::<U0,f32,u8>::new(2.0,1).to_expr();
-/// let y = x.exp();
-/// let v = arr![f32; 1., 1.];
-/// let v1 = arr![f32; 2., 3.];
-/// assert_eq!(2.0_f32.exp(),y.calc(v));
-/// assert_eq!(4.0_f32.exp(),y.calc(v1));
-/// assert_eq!(2.0_f32.exp()*2.0,y.diff(U0::new()).calc(v));
-/// assert_eq!(4.0_f32.exp()*2.0,y.diff(U0::new()).calc(v1));
-/// ```
-pub type ExpSym<Sym, Out, In> = UnarySym<ExpOp, Sym, Out, In>;
-
-impl<Sym, Out, In> Symbol<Out, In> for ExpSym<Sym, Out, In>
-where
-    Sym: Symbol<Out, In>,
-    Out: Float,
-{
-    type Derivative = impl Symbol<Out, In>; // MulSym<Sym::Derivative, ExpSym<Sym, Out, In>,Out,In>;
-    fn calc_ref(&self, value: &In) -> Out {
-        self.sym.calc_ref(value).exp()
-    }
-    fn diff<Dm>(&self, dm: Dm) -> <Self as Symbol<Out, In>>::Derivative
-    where
-        Dm: DiffMarker,
-    {
-        (self.sym.diff(dm).to_expr() * self.clone()).0
-    }
-}
-*/
 
 macro_rules! FlaotSymbols {
     ( $( ($t:ident,$me:ident,$op:ident,$ex:tt); )* $(;)*  ) => {
@@ -52,11 +11,6 @@ macro_rules! FlaotSymbols {
         where
             Out: Float,
         {
-            /*
-            fn exp(self) -> ExpSym<Self, Out, In> {
-                self.into()
-            }
-            */
             $(
                 fn $me(self) -> UnarySym<$op, Self, Out, In> {
                     self.into()
@@ -100,25 +54,6 @@ FlaotSymbols!(
     (Cos,cos, CosOp, (|x : &Self| -(x.sym.clone().sin().to_expr())) );
     (Tan,tan, TanOp, (|x : &Self| { let cos = x.sym.clone().cos(); Const(Out::one()).to_expr() / (cos.clone().to_expr() * cos).inner() } ));
 );
-
-//FloatOps!(Sin,sin, SinOp, (|x : Sym| x.cos()) );
-
-/*
-pub trait FloatSymbolEx<Out, In>: Symbol<Out, In>
-where
-    Out: Float,
-{
-    fn exp(self) -> ExpSym<Self, Out, In> {
-        self.into()
-    }
-    fn sin(self) -> UnarySym<SinOp, Self, Out, In> {
-        self.into()
-    }
-    fn cos(self) -> UnarySym<CosOp, Self, Out, In> {
-        self.into()
-    }
-}
-*/
 
 impl<Sym, Out, In> UnaryFloatSymbolEx<Out, In> for Sym
 where
